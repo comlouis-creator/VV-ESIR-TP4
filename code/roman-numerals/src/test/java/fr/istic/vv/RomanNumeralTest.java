cb0cfdb28f1a3d8ff8792d5f1e0e140dcd74bc7a
Louis MORLOT--PINTA <76203767+comlouis-creator@users.noreply.github.com>
2022-12-09 22:34:54 +0100

    property based tests using jqwik

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,36 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.arbitraries.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralTest {
     @Property
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
+    
+    @Property
+    void isValidRomanNumeral(@ForAll("validRomanNumerals") String numeral) {
+        assertEquals(true, RomanNumeralUtils.isValidRomanNumeral(numeral));
+    }
+
+    @Property
+    void parseRomanNumeral(@ForAll("validRomanNumerals") String numeral) {
+        assertEquals(numeral, RomanNumeralUtils.toRomanNumeral(RomanNumeralUtils.parseRomanNumeral(numeral)));
+    }
+
+    @Property
+    void toRomanNumeral(@ForAll int number) {
+        if (number >= 1 && number <= 3999) {
+            assertEquals(number, RomanNumeralUtils.parseRomanNumeral(RomanNumeralUtils.toRomanNumeral(number)));
+        }
+    }
+
+    @Provide
+    Arbitrary<String> validRomanNumerals() {
+        return Arbitraries.of("I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M")
+                          .flatMap(s -> Arbitraries.oneOf(s, s + s, s + s + s));
+    }
+
 }
