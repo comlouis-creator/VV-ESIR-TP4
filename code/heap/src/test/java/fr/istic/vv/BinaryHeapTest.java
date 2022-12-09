b26a4d60f4581647b056247cc48bd699ce426a91
Louis MORLOT--PINTA <76203767+comlouis-creator@users.noreply.github.com>
2022-12-09 23:01:53 +0100

    ajout tests jqwik binaryheap

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -7,4 +7,19 @@ public class BinaryHeapTest {
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
+    
+    private BinaryHeap<Integer> heap;
+
+    @BeforeEach
+    void setUp() {
+        heap = new BinaryHeap<>(Comparator.naturalOrder());
+    }
+
+    @Property
+    void popReturnsMinimumElement(@ForAll int element) {
+        heap.push(element);
+        int minElement = Collections.min(heap.getElements());
+        assertEquals(minElement, heap.pop());
+    }
+    
 }
